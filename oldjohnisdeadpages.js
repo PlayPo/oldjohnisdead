ef422fde6d391fda6d8691891dd41cfd90b0fc85
PlayPo <juegapo2022@gmail.com>
2024-07-22 22:12:39 -0300

    Add files via upload

---
+++ oldjohnisdeadpages.js
@@ -0,0 +1,48 @@
+var i; 
+
+var arLinks = new Array();
+
+arLinks[0] = "index.html";
+arLinks[1] = "neverending_dream.html";
+arLinks[2] = "in_my_head.html";
+arLinks[3] = "getout.html";
+arLinks[4] = "lost_in_my_mind.html";
+arLinks[5] = "misleading.html";
+arLinks[6] = "underground.html";
+arLinks[7] = "cant_swim.html";
+arLinks[8] = "wool.html";
+arLinks[9] = "fantasy.html";
+arLinks[10] = "vice.html";
+arLinks[11] = "mister.html";
+arLinks[12] = "breathe.html";
+arLinks[13] = "blood.html";
+arLinks[14] = "asleep.html";
+arLinks[15] = "down.html";
+arLinks[16] = "lost_forever.html"
+arLinks[17] = "occasions.html"
+arLinks[18] = "fakeworld.html"
+arLinks[19] = "blue_flame.html"
+arLinks[20] = "imagine.html"
+arLinks[21] = "hold_on.html"
+arLinks[22] = "again.html"
+arLinks[23] = "oooooo.html"
+arLinks[24] = "faking.html"
+arLinks[25] = "try.html"
+arLinks[26] = "destroy.html"
+arLinks[27] = "say_goodbye.html"
+arLinks[28] = "anotherone.html"
+arLinks[29] = "help.html"
+arLinks[30] = "head.html"
+arLinks[31] = "mute.html"
+arLinks[32] = "years.html"
+arLinks[33] = "without.html"
+arLinks[34] = "equa.html"
+arLinks[35] = "me_and_john.html"
+arLinks[36] = "flesh.html"
+arLinks[37] = "go_back.html"
+arLinks[38] = "enter.html"
+arLinks[39] = "notbreathe.html"
+arLinks[40] = "diarys.html"
+
+i = Math.floor( Math.random() * arLinks.length );
+window.location = arLinks[i];
